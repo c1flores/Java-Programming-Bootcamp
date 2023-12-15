b69bd581d65b3bee338afb6923dd0246db1d4026
Christian Flores <c1flores@ucsd.edu>
2023-12-15 09:56:41 -0800

    Add code examples for second learning module

---
+++ Course Pages/Java Application Structure/main/java/structure/Account.java
@@ -0,0 +1,28 @@
+package main.java.structure;
+
+public class Account {
+    String owner;
+    double balance;
+    double minimumBalance;
+
+    public void deposit(double amount) {
+        // implementation
+    }
+
+    public void withdraw(double amount) {
+        // implementation
+    }
+
+    public double calculateCreditWorthiness() {
+        // implementation
+        return 100;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String number) {
+        this.owner = owner;
+    }
+}
